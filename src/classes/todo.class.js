e4614cfd47c376babed0f04cde41bd6fcbf09489
salazarkendall <kendallsalazarvargas@gmail.com>
2021-04-07 17:17:38 -0600

    App v1.2

--- src/classes/todo.class.js
+++ src/classes/todo.class.js
@@ -2,6 +2,8 @@
  * Esta clase es como la tipica clase de java
  * Una clase pojo con los valores que debe de tener a la hora de construirse
  * ID es generado de manera automatica con un codigo casi irrepetible
+ * 
+ * Podemos notar que esta clase tiene 2 formas de generarse pero aparte de eso, no tiene nada mas
  */
 export class Todo {
 
