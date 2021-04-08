dbc6eee0adeea8591906372c10e3fd9f0035aad7
kndx99 <kendallsalazarvargas@gmail.com>
2021-04-07 22:13:53 -0600

    App v1.2.1

--- src/classes/todo-list.class.js
+++ src/classes/todo-list.class.js
@@ -1,63 +1,63 @@
-import { Todo } from "./todo.class";
-import { contador } from "../js/componentes.js";
+import { Todo } from './todo.class';
+import { contador } from '../js/componentes.js';
 
 /**
  * Esta clase tiene un array de todos, sirve como un repositorio en el que guardaremos los todos
  * Ademas de las funcionabilidades basicas que tiene la lista de tareas
  */
 export class TodoList {
-    constructor() {
-        this.cargarLocalStorage();
-        this.contarTodos()
-    }
+  constructor() {
+    this.cargarLocalStorage();
+    this.contarTodos();
+  }
 
-    nuevoTodo(todo) {
-        this.todos.push(todo);
+  nuevoTodo(todo) {
+    this.todos.push(todo);
+    this.guardarLocalStorage();
+    this.contarTodos();
+  }
+
+  eliminarTodo(id) {
+    this.todos = this.todos.filter((todo) => todo.id != id);
+    this.guardarLocalStorage();
+    this.contarTodos();
+  }
+
+  marcarCompletado(id) {
+    for (const todo of this.todos) {
+      if (todo.id == id) {
+        todo.completado = !todo.completado;
         this.guardarLocalStorage();
-        this.contarTodos()
+        this.contarTodos();
+        break;
+      }
     }
+  }
 
-    eliminarTodo(id) {
-        this.todos = this.todos.filter((todo) => todo.id != id);
-        this.guardarLocalStorage();
-        this.contarTodos()
+  borrarCompletados() {
+    this.todos = this.todos.filter((todo) => !todo.completado);
+    this.guardarLocalStorage();
+  }
+
+  guardarLocalStorage() {
+    localStorage.setItem('todo', JSON.stringify(this.todos));
+  }
+
+  cargarLocalStorage() {
+    this.todos = localStorage.getItem('todo')
+      ? JSON.parse(localStorage.getItem('todo'))
+      : [];
+    this.todos = this.todos.map(Todo.fromJson);
+  }
+
+  contarTodos() {
+    let pendientes = 0;
+    let tempContador = contador.firstElementChild;
+    for (const todo of this.todos) {
+      if (!todo.completado) {
+        pendientes++;
+      }
     }
-
-    marcarCompletado(id) {
-        for (const todo of this.todos) {
-            if (todo.id == id) {
-                todo.completado = !todo.completado;
-                this.guardarLocalStorage();
-                this.contarTodos()
-                break;
-            }
-        }
-
-    }
-
-    borrarCompletados() {
-        this.todos = this.todos.filter((todo) => !todo.completado);
-        this.guardarLocalStorage();
-    }
-
-    guardarLocalStorage() {
-        localStorage.setItem('todo', JSON.stringify(this.todos));
-    }
-
-    cargarLocalStorage() {
-        this.todos = localStorage.getItem('todo') ?
-            JSON.parse(localStorage.getItem('todo')) : [];
-        this.todos = this.todos.map(Todo.fromJson)
-    }
-
-    contarTodos() {
-        let pendientes = 0;
-        let tempContador = contador.firstElementChild
-        for (const todo of this.todos) {
-            if (!todo.completado) {
-                pendientes++
-            }
-        }
-        tempContador.innerHTML = pendientes
-    }
+    tempContador.innerHTML = pendientes;
+  }
 }
--- src/classes/todo.class.js
+++ src/classes/todo.class.js
@@ -2,24 +2,25 @@
  * Esta clase es como la tipica clase de java
  * Una clase pojo con los valores que debe de tener a la hora de construirse
  * ID es generado de manera automatica con un codigo casi irrepetible
- * 
+ *
  * Podemos notar que esta clase tiene 2 formas de generarse pero aparte de eso, no tiene nada mas
  */
 export class Todo {
+  static fromJson({ tarea, id, completado, creado }) {
+    const tempTodo = new Todo(tarea);
+    tempTodo.id = id;
+    tempTodo.completado = completado;
+    tempTodo.creado = creado;
 
-    static fromJson({ tarea, id, completado, creado }) {
-        const tempTodo = new Todo(tarea)
-        tempTodo.id = id
-        tempTodo.completado = completado
-        tempTodo.creado = creado
+    return tempTodo;
+  }
 
-        return tempTodo
-    }
-
-    constructor(tarea) {
-        this.tarea = tarea;
-        this.id = new Date().getTime();
-        this.completado = false;
-        this.creado = new Date();
-    }
+  constructor(tarea) {
+    this.tarea = tarea;
+    this.id = new Date().getTime();
+    this.completado = false;
+    this.creado = new Date();
+    this.id = 4;
+  }
+  // "editor.codeActionsOnSave": true,
 }
